09f56e55d6a5d12e27520aede4e78745860db682
Emmanuel Ruiz <ruiizemmanuel@gmail.com>
2020-09-16 02:06:31 -0500

    Get posts from queries using apollo

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,28 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Container, GridListTile, GridList } from "@material-ui/core";
+import React from "react";
+import { Container, Grid } from "@material-ui/core";
+
+import PostCard from "../components/PostCard";
+import { GET_POSTS } from "../util/graphql/queries";
+import { useQuery } from "@apollo/client";
 
 function Home() {
+  const { loading, error, data } = useQuery(GET_POSTS);
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  const posts = data.getPosts;
+
   return (
     <Container>
-      <div>Home</div>
+      <Grid container>
+        {posts &&
+          posts.map((post) => (
+            <Grid item xs={12} sm={6} md={4} key={post._id}>
+              <PostCard post={post} />
+            </Grid>
+          ))}
+      </Grid>
     </Container>
   );
 }
