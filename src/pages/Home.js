0c67098b7e52dc447322325176dc48ecd1211793
Emmanuel Ruiz <ruiizemmanuel@gmail.com>
2020-09-15 20:39:09 -0500

    Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, GridListTile, GridList } from "@material-ui/core";
+
+function Home() {
+  return (
+    <Container>
+      <div>Home</div>
+    </Container>
+  );
+}
+
+export default Home;
