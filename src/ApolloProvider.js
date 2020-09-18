4480777caa4e18faa87cd884ea696f546931a6ee
Emmanuel Ruiz <ruiizemmanuel@gmail.com>
2020-09-17 23:29:42 -0500

    Apollo links added to the provider

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -2,20 +2,22 @@ import React from "react";
 import App from "./App";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
+import { ApolloLink } from "apollo-link";
 import { createHttpLink } from "apollo-link-http";
 import { setContext } from "apollo-link-context";
+import { onError } from "apollo-link-error";
 
 const httpLink = createHttpLink({
-  uri: "https://share-images-reh4m.herokuapp.com/graphql",
+  uri: process.env.REACT_APP_SERVER_URI,
 });
 
 const authLink = setContext(() => {
-  // If no token with key of "token" in localStorage, add it
+  // If no token with key of "token" in localStorage, add it.
   if (!localStorage.token) {
     localStorage.setItem("token", "");
   }
 
-  // Operation adds the token to an auth header, which is sent to backend
+  // Operation adds the token to an auth header, which is sent to backend.
   return {
     headers: {
       Authorization: localStorage.getItem("token"),
@@ -23,10 +25,24 @@ const authLink = setContext(() => {
   };
 });
 
-console.log(authLink.concat(httpLink));
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (networkError) {
+    console.log("[networkError]", networkError);
+  }
+  if (graphQLErrors) {
+    for (let err of graphQLErrors) {
+      console.dir(err);
+      // if (err.name === 'AuthenticationError') {
+
+      // }
+    }
+  }
+});
+
+const link = ApolloLink.from([httpLink, authLink, errorLink]);
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache(),
 });
 
