27c0b4afd2ba3e812f8ade07888a104335b84b93
Emmanuel Ruiz <ruiizemmanuel@gmail.com>
2020-09-20 23:36:08 -0500

    Log in buttons on the Header

--- src/layouts/Header.js
+++ src/layouts/Header.js
@@ -1,15 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
 import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  buttons: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -19,12 +31,39 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function Header() {
+export default function Header() {
   const classes = useStyles();
+  const [auth, setAuth] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleChange = (event) => {
+    setAuth(event.target.checked);
+  };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
-    <div>
-      <AppBar position="static">
+    <div className={classes.root}>
+      {/* <FormGroup>
+        <FormControlLabel
+          control={
+            <Switch
+              checked={auth}
+              onChange={handleChange}
+              aria-label="login switch"
+            />
+          }
+          label={auth ? "Logout" : "Login"}
+        />
+      </FormGroup> */}
+      <AppBar>
         <Toolbar>
           <IconButton
             edge="start"
@@ -37,13 +76,59 @@ function Header() {
           <Typography variant="h6" className={classes.title}>
             React Posts
           </Typography>
-          <IconButton aria-label="search" color="inherit">
-            <SearchIcon />
-          </IconButton>
+          {auth ? (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "right",
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          ) : (
+            <div className={classes.buttons}>
+              <Button
+                size="small"
+                color="inherit"
+                component={Link}
+                to="/signin"
+              >
+                Sign in
+              </Button>
+              <Button
+                size="small"
+                variant="outlined"
+                color="inherit"
+                component={Link}
+                to="/signup"
+              >
+                Sign up
+              </Button>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
     </div>
   );
 }
-
-export default Header;
