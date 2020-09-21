f4def59483b02b1a94499b27ffa7392411ea0fa0
Emmanuel Ruiz <ruiizemmanuel@gmail.com>
2020-09-21 00:07:35 -0500

    Added spacing to the main content into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
 
 import Header from "./layouts/Header";
 import Home from "./pages/Home";
 
+const useStyles = makeStyles((theme) => ({
+  mainContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
+
   return (
     <Router>
       <Header />
-      <Route exact path="/" component={Home} />
+      <main className={classes.mainContent}>
+        <Route exact path="/" component={Home} />
+      </main>
     </Router>
   );
 }
